114aed709002afc2725c6a832089335d84bcbb6e
Damian Nolan <damiannolan@gmail.com>
2017-12-18 21:53:47 +0000

    sleeping thread to simulate delay

--- src/main/resources/ie/gmit/sw/RMIClient.java
+++ src/main/resources/ie/gmit/sw/RMIClient.java
@@ -3,6 +3,12 @@ package ie.gmit.sw;
 public class RMIClient implements Runnable {
 
 	public void run() {
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println("Running in another thread");
 	}
 
